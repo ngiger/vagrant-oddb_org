2c75f5b95d45fa3ed191edd6c570354f9da261c1
Niklaus Giger <niklaus.giger@member.fsf.org>
2013-04-13 11:54:30 +0200

    Add ruby-password to be able use sha256.rb

--- static-modules/oddb_org/files/install_yus.sh
+++ static-modules/oddb_org/files/install_yus.sh
@@ -4,7 +4,7 @@ export RUBYOPT="-rauto_gem"
 # We want to ensure that the following three gems are installed via emerge for gem18
 gem18 uninstall -x --all dbi dbd-pg pg 
 gem18 install --no-ri --no-rdoc deprecated --version=2.0.1
-gem18 install --no-ri --no-rdoc yus rclconf odba needle
+gem18 install --no-ri --no-rdoc yus rclconf odba needle ruby-password
 emerge =dev-ruby/dbi-0.4.3 =dev-ruby/dbd-pg-0.3.9 =dev-ruby/pg-0.11.0
 gem18 list --local
 head /usr/local/bin/yus* | grep bin >/opt/yus_installed.log
